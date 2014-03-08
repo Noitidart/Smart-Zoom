ba6accff7f157f0ca36a4f9b564138b6908c8723
Noitidart <noitidart@gmail.com>
2014-03-08 01:52:08 -0800

    initial
    set up boiler plate and icon

---
+++ bootstrap.js
@@ -0,0 +1,72 @@
+const {interfaces: Ci, utils: Cu} = Components;
+const self = {
+	id_prefix: 'Zoomr',
+	id_suffix: '@jetpack',
+	name: 'Zoomr',
+	aData: 0,
+};
+
+Cu.import('resource://gre/modules/Services.jsm');
+
+/*start - windowlistener*/
+var windowListener = {
+	//DO NOT EDIT HERE
+	onOpenWindow: function (aXULWindow) {
+		// Wait for the window to finish loading
+		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		aDOMWindow.addEventListener("load", function () {
+			aDOMWindow.removeEventListener("load", arguments.callee, false);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}, false);
+	},
+	onCloseWindow: function (aXULWindow) {},
+	onWindowTitleChange: function (aXULWindow, aNewTitle) {},
+	register: function () {
+		// Load into any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}
+		// Listen to new windows
+		Services.wm.addListener(windowListener);
+	},
+	unregister: function () {
+		// Unload from any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.unloadFromWindow(aDOMWindow, aXULWindow);
+		}
+		//Stop listening so future added windows dont get this attached
+		Services.wm.removeListener(windowListener);
+	},
+	//END - DO NOT EDIT HERE
+	loadIntoWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		
+	},
+	unloadFromWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+	}
+};
+/*end - windowlistener*/
+function startup(aData, aReason) {
+	//self.aData = aData; //must go first, because functions in loadIntoWindow use self.aData
+	windowListener.register();
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) return;
+	windowListener.unregister();
+}
+
+function install() {}
+
+function uninstall() {}
